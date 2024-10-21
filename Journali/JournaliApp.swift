d35b423754616293face52f53b190a8de33a1464
shfa <shefaa.eidaroos@gmail.com>
2024-10-21 17:38:27 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Whyyy on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

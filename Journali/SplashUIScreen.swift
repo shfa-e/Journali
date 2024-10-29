29ad956951a3b5492e7b6df4d7e47f03b60f65f3
shfa <shefaa.eidaroos@gmail.com>
2024-10-29 08:43:02 +0300

    Added splash screen

---
+++ Journali/SplashUIScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashUIScreen.swift
+//  Journali
+//
+//  Created by Whyyy on 29/10/2024.
+//
+
+import SwiftUI
+
+struct SplashUIScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashUIScreen()
+}
